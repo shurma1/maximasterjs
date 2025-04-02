70868c54a6b58aed6d86f31135af305454f20d53
shurma1 <dmitriy.prokhorov.04@mail.ru>
2025-04-02 14:26:50 +0300

    setting eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,24 +5,39 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+	{ ignores: ['dist'] },
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
+			'indent': ['error', 'tab'],
+			'quotes': ['error', 'single'],
+			'no-trailing-spaces': ['error'],
+			'max-len': ['error', {
+				code: 120,
+				ignoreUrls: true,
+				ignoreStrings: true,
+				ignoreTemplateLiterals: true,
+				ignoreRegExpLiterals: true
+			}],
+			'no-multiple-empty-lines': ['error', {
+				max: 1,
+				maxEOF: 0
+			}],
+			'semi': ['error', 'always'],
+		},
+	},
 )
